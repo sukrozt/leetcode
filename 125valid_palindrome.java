fde20a1bc28c643fd66f3e78db4249d6f6d406ac
sukrozt <sukriyeo13@gmail.com>
2025-09-26 21:15:08 +0300

    add 125

---
+++ 125valid_palindrome.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[\\s\\p{Punct}]+", "");
+        if (s.trim().isEmpty()) return true;
+
+        int i = 0;
+        int j = s.length()-1;
+
+        while(i < j){
+            if(s.charAt(i) != s.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
